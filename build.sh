f1919964c126ef2766e0a816761c505cb5f7bb14
MD KHIZER <mdkhizer15@gmail.com>
2025-10-31 18:37:45 +0530

    Fix build.sh to remove apt-get commands that fail on Render

--- build.sh
+++ build.sh
@@ -1,17 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Install system dependencies for Pillow
-apt-get update
-apt-get install -y \
-    python3-dev \
-    python3-pip \
-    python3-setuptools \
-    libjpeg-dev \
-    zlib1g-dev
-
-# Upgrade pip
+# Upgrade pip first
 python -m pip install --upgrade pip
 
-# Install Python packages
+# Install Python packages (Pillow will handle its own dependencies)
 pip install --no-cache-dir -r requirements.txt
