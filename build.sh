8440558219fc9d5d0bdbc5e6dbbae1378ad9cd6a
MD KHIZER <mdkhizer15@gmail.com>
2025-10-31 14:45:33 +0530

    Fix deployment configuration and dependencies

--- build.sh
+++ build.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-# Install system dependencies for Pillow
-apt-get update
-apt-get install -y python3-dev python3-pip python3-venv
-apt-get install -y libtiff5-dev libjpeg62-turbo-dev libopenjp2-7-dev zlib1g-dev \
-    libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python3-tk \
-    libharfbuzz-dev libfribidi-dev libxcb1-dev
+# Update pip
+python -m pip install --upgrade pip
 
-# Install Python dependencies
-pip install --upgrade pip
+# Install system dependencies
+apt-get update && apt-get install -y \
+    python3-dev \
+    python3-pip \
+    python3-venv \
+    gcc \
+    libjpeg-dev \
+    zlib1g-dev
+
+# Install Python packages
 pip install -r requirements.txt
