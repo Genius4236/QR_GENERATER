2a9dca013cdf85abd628085890e4b52c0fd48295
MD KHIZER <mdkhizer15@gmail.com>
2025-10-31 14:32:08 +0530

    render deployment

--- app.py
+++ app.py
@@ -29,3 +29,8 @@ if __name__ == '__main__':
     if not os.path.exists('static'):
         os.makedirs('static')
     app.run(debug=True)
+
+if __name__ == '__main__':
+    # Use environment variable for port
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
